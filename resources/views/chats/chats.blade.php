186508124d9148944016809765fa22d672901047
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-12-01 14:37:27 +0700

    fix-style

--- resources/views/chats/chats.blade.php
+++ resources/views/chats/chats.blade.php
@@ -57,8 +57,8 @@
   <div class="col-md-12 alert alert-warning">{{ $error }}</div>
 @endif
 
- <!--  elseif($row['val']['type'] == 'document')
-              <audio controls>
-                <source src="{{url('get_media')}}/{{ $app->media_link_parse($row['val']['media_url']) }}/audio" type="audio/ogg">
-              Your browser does not support the audio element.
-              </audio> -->
+ <!-- if(row['val']['type'] == 'document')
+  <audio controls>
+    <source src="">
+  Your browser does not support the audio element.
+  </audio> -->
