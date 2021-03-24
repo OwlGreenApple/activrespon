8f711410a41590903944948a3f6aebac4af0e6b2
rizky redjo <celebgramme.dev@gmail.com>
2021-03-24 12:59:35 +0700

    growing rich wp add cron

--- app/Http/Controllers/ApiWPController.php
+++ app/Http/Controllers/ApiWPController.php
@@ -427,6 +427,80 @@ class ApiWPController extends Controller
       }
     }
   
+    public function send_message_queue_system_WP_growingrich(Request $request)
+    {
+      if ($request->key == "wpcallbackforwa" ) {
+				$str = strip_tags($request->phone);
+        $phone_number = strip_tags($request->phone);
+        $name = strip_tags($request->name);
+        $email = strip_tags($request->email);
+        $content = strip_tags($request->content);
+        
+				if(preg_match('/^62[0-9]*$/',$str)){
+          $phone_number = '+'.$str;
+        }
+
+        if(preg_match('/^0[0-9]*$/',$str)){
+          $phone_number = preg_replace("/^0/", "+62", $str);
+        }
+
+        if(preg_match('/^[^62][0-9]*$/',$str)){
+          $phone_number = preg_replace("/^[0-9]/", "+62", $str);
+        }
+
+        $list = UserList::where('name',"phoicwfb")->first();
+        if ($request->event == "checkout-completed"){
+          //list khusus michaelsugiharto
+
+          if (!is_null($list)) {
+            $customer_phone = Customer::where([['list_id',$list->id],['telegram_number',$phone_number]])->first();
+
+            if(is_null($customer_phone))
+            {
+              $customer = new Customer ;
+              $customer->user_id = $list->user_id;
+              $customer->list_id = $list->id;
+              $customer->name = $name;
+              $customer->email = $email;
+              $customer->telegram_number = $phone_number;
+              $customer->is_pay= 0;
+              $customer->status = 1;
+              $customer->save();
+              $customer::create_link_unsubs($customer->id,$list->id);
+
+              $customerController = new CustomerController;
+              if ($list->is_secure) {
+                $ret = $customerController->sendListSecure($list->id,$customer->id,$name,$customer->user_id,$list->name,$phone_number);
+              }
+              $saveSubscriber = $customerController->addSubscriber($list->id,$customer->id,$customer->created_at,$customer->user_id);
+            }
+          }
+        }
+
+        $phoneNumber = PhoneNumber::where('user_id',$list->user_id)->first();
+        $key = env('REMINDER_PHONE_KEY');
+        $mode = 0;
+        if (!is_null($phoneNumber)){
+          if ($phoneNumber->mode == 0){ //simi
+            $server = Server::where('phone_id',$phoneNumber->id)->first();
+            $key = $server->url;
+            $mode = 0;
+          }
+          if ($phoneNumber->mode == 1){ //woowa
+            $key = $phoneNumber->filename;
+            $mode = 1;
+          }
+          if ($phoneNumber->mode == 2){ //wamate
+            $key = $phoneNumber->device_key;
+            $mode = 2;
+          }
+        }
+        $message_send = Message::create_message($phone_number,$content,$key,$mode);
+
+        return "success";
+      }
+    }
+  
     public function sendToCelebmail($name,$email,$list_unique_id)
     {
       $fname = "";
